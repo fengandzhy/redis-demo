3f401d00d32454811a77fa3b655f58002b613e1a
fengandzhy <fengandzhy@gmail.com>
2021-07-11 20:56:02 +1200

    redis-string-1

---
+++ springboot-redis-G/redis-string-1/src/main/java/org/zhouhy/redis/StringApp.java
@@ -0,0 +1,15 @@
+package org.zhouhy.redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 1. 启动redis命令./src/redis-server /usr/zhouhy/redis-5.0.7/redis.conf 
+ * 2. 停止redis命令 ./src/redis-cli -h 127.0.0.1 -p 6379 shutdown
+ * */
+@SpringBootApplication
+public class StringApp {
+    public static void main(String[] args) {
+        SpringApplication.run(StringApp.class,args);
+    }
+}
